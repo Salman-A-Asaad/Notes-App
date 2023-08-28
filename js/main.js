158f8db317d8fa78fab849a0c9495c7eb8f48903
slman5asaad <slman.github@gmail.com>
2023-08-28 11:42:10 -0400

    Create Notes-App repo

---
+++ js/main.js
@@ -0,0 +1,32 @@
+let container = document.getElementsByClassName("container")[0];
+
+function show() {
+  container.innerHTML = localStorage.note;
+}
+show();
+let button = document.getElementById("button");
+button.addEventListener("click", () => {
+  let note = document.createElement("div");
+  let textArea = document.createElement("textarea");
+  let img = document.createElement("img");
+  note.classList.add("note");
+  textArea.setAttribute("placeholder", "Write");
+  img.src = "img/delete.png";
+  note.appendChild(textArea);
+  note.appendChild(img);
+  container.appendChild(note);
+});
+container.addEventListener("click", (e) => {
+  if (e.target.tagName == "IMG") {
+    e.target.parentElement.remove();
+    localStorage.note = container.innerHTML;
+  } else if (e.target.tagName == "TEXTAREA") {
+    let note = Array.from(document.querySelectorAll(".note textarea"));
+    note.forEach((e) => {
+      e.onkeyup = function () {
+        e.textContent = e.value;
+        localStorage.note = container.innerHTML;
+      };
+    });
+  }
+});
